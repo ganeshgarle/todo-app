99322658cf193672bf235b3eb752f903c99b1a94
Ganesh Garle <ganeshgarle@Ganeshs-MacBook-Pro.local>
2018-07-30 23:25:43 +0530

    Added basic crousel

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Button } from 'reactstrap';
+
 import './App.css';
+
+import Header from './Header'
+import HomePageCrousel from './Carousel/HomePage'
 
 class App extends Component {
   constructor(props){
     super(props);
-    this.clickMe = this.clickMe.bind(this);
   }
-  clickMe(){
-    console.log('clickMe');
-  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-         <Button color="danger">Danger!</Button>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <div className="row Carousel-home">
+          <div>
+            <HomePageCrousel />
+          </div>
+        </div>
       </div>
     );
   }
